bbe81fd558dc7c9f0f2d41c11e6071366e522460
a738271123 <738271123@qq.com>
2018-10-26 08:08:50 +0800

    000

--- 00.py
+++ 00.py
@@ -1,2 +1,10 @@
-print("dming");
-print("i am king");
+print(r'''hello,\n
+world''')
+
+print('''line1
+line2
+line3''')
+print("i am king")
+name = input("输入你的名字")
+print(name,", hello")
+print("1024 * 768 = xxx")
