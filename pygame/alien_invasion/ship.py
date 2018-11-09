8183527be44103a541e20a8901651744141be8ea
a738271123 <738271123@qq.com>
2018-11-09 13:24:55 +0800

    dming1

--- pygame/alien_invasion/ship.py
+++ pygame/alien_invasion/ship.py
@@ -1,6 +1,16 @@
 import pygame
 class Ship():
-    def __init__():
+    def __init__(self,screen):
         self.screen = screen
-        self.image = pygame.image.load("image/ship.bmp")
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
+        self.moving_right = False
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx +=1
